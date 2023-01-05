da4879b76a4c878dff260012d325695cf3281cc2
renagge <63189759+renagge@users.noreply.github.com>
2023-01-05 08:53:18 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from flask import Flask, request, session, redirect, url_for, render_template, flash
-import psycopg2 #pip install psycopg2 
+import psycopg2  
 import psycopg2.extras
 import re 
 from werkzeug.security import generate_password_hash, check_password_hash
- 
+from functools import wraps
+import urllib.request
+from datetime import datetime
+from werkzeug.utils import secure_filename
+import os
+
 app = Flask(__name__)
-app.secret_key = 'cairocoders-ednalan'
- 
+app.secret_key = 'kelompok4'
+#=============================================DAATABASE=================================
 DB_HOST = "localhost"
-DB_NAME = "sampledb"
+DB_NAME = "test"
 DB_USER = "postgres"
 DB_PASS = "170845"
  
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
- 
+
+#===========================================================Routing Basic Layout=====================================================================   
+
 @app.route('/')
 def home():
     # Check if user is loggedin
@@ -23,53 +30,35 @@ def home():
         return render_template('home.html', username=session['username'])
     # User is not loggedin redirect to login page
     return redirect(url_for('index'))
-@app.route('/upload/',methods=["GET","POST"])
-def upload():
-    if request.method == "GET":
-        return render_template("upload.html")
-    elif request.method == "POST":
-        request.files.get("file")
-        return redirect(url_for('home'))
         
 @app.route('/index/')
 def index():
      return render_template('index.html')
  
-@app.route('/login/', methods=['GET', 'POST'])
-def login():
+@app.route('/storage')
+def storage():
+    if 'loggedin' in session:
+    
+        # User is loggedin show them the home page
+        return render_template('storage.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+  
+@app.route('/profile', methods=['GET','POST'])
+def profile(): 
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
    
-    # Check if "username" and "password" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        username = request.form['username']
-        password = request.form['password']
-        print(password)
- 
-        # Check if account exists using MySQL
-        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
-        # Fetch one record and return result
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        cursor.execute('SELECT * FROM users WHERE id = %s', [session['id']])
         account = cursor.fetchone()
- 
-        if account:
-            password_rs = account['password']
-            print(password_rs)
-            # If account exists in users table in out database
-            if check_password_hash(password_rs, password):
-                # Create session data, we can access this data in other routes
-                session['loggedin'] = True
-                session['id'] = account['id']
-                session['username'] = account['username']
-                # Redirect to home page
-                return redirect(url_for('home'))
-            else:
-                # Account doesnt exist or username/password incorrect
-                flash('Incorrect username/password')
-        else:
-            # Account doesnt exist or username/password incorrect
-            flash('Incorrect username/password')
- 
-    return render_template('login.html')
-  
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
+#=================================================================ROUTING ACONT=============================================================
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
@@ -108,7 +97,45 @@ def register():
         flash('Please fill out the form!')
     # Show registration form with message (if any)
     return render_template('register.html')
+
+
+ 
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
    
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        print(password)
+ 
+        # Check if account exists using MySQL
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+ 
+        if account:
+            password_rs = account['password']
+            print(password_rs)
+            # If account exists in users table in out database
+            if check_password_hash(password_rs, password):
+                # Create session data, we can access this data in other routes
+                session['loggedin'] = True
+                session['id'] = account['id']
+                session['username'] = account['username']
+                # Redirect to home page
+                return redirect(url_for('home'))
+            else:
+                # Account doesnt exist or username/password incorrect
+                flash('Incorrect username/password')
+        else:
+            # Account doesnt exist or username/password incorrect
+            flash('Incorrect username/password')
+ 
+    return render_template('login.html')
+
+
    
 @app.route('/logout')
 def logout():
@@ -118,19 +145,107 @@ def logout():
    session.pop('username', None)
    # Redirect to login page
    return redirect(url_for('login'))
-  
-@app.route('/profile')
-def profile(): 
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+#==================================================================Routing Feature ============================================================
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
    
-    # Check if user is loggedin
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+   
+def allowed_file(filename):
+ return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload/',methods=["GET","POST"])
+def upload():
+    if request.method == "GET":
+        return render_template("upload.html")
+    elif request.method == "POST":
+        request.files.get("file")
+        return redirect(url_for('home'))
+
+
+@app.route("/upload",methods=["POST","GET"])
+def upload_storage():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    now = datetime.now()
+    print(now)
+    if request.method == 'POST':
+        files = request.files.getlist('files[]')
+        #print(files)
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                cur.execute("INSERT INTO images (file_name, uploaded_on) VALUES (%s, %s)",[filename, now])
+                conn.commit()
+            print(file)
+        cur.close()   
+        flash('File(s) successfully uploaded')    
+    return redirect('/storage')
+@app.route('/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+
+#============================================================Routing Dahsboard=================================================================
+@app.route('/post',methods=['POST','POST'])
+def post():
     if 'loggedin' in session:
-        cursor.execute('SELECT * FROM users WHERE id = %s', [session['id']])
-        account = cursor.fetchone()
-        # Show the profile page with account info
-        return render_template('profile.html', account=account)
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        if request.method == 'POST' and 'tittle_post' in request.form and 'body_post' in request.form and 'date_post' in request.form:
+            tittle_post = request.form['tittle_post']
+            body_post = request.form['body_post']
+            date_post = request.form['date_post']
+            cur.execute("INSERT INTO post (tittle_post,body_post,date_post) VALUES (%s,%s,%s)",(tittle_post,body_post,date_post))
+            conn.commit()
+            flash('Post Added successfully')
+        return redirect(url_for('home'))
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
- 
+
+@app.route('/edit/<id>', methods = ['POST', 'GET'])
+def get_post(id):
+   cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+   if 'loggedin' in session:
+            cur.execute('SELECT * FROM post WHERE id = %s', (id))
+            data = cur.fetchall()
+            cur.close()
+            print(data[0])
+            return render_template('edit.html', post = data[0])
+            return redirect(url_for('login'))
+
+@app.route('/update/<id>', methods=['POST'])
+def update_post(id):
+    if 'loggedin' in session:
+        if request.method == 'POST':
+            tittle_post = request.form['tittle_post']
+            body_post = request.form['body_post']
+            date_post = request.form['date_post']
+         
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("""
+            UPDATE post
+            SET tittle_post = %s,
+                body_post = %s,
+                date_post = %s
+            WHERE id = %s
+        """, (tittle_post, body_post, date_post, id))
+        flash('Post Updated Successfully')
+        conn.commit()
+        return redirect(url_for('Index'))
+    return redirect(url_for('login'))
+
+@app.route('/delete/<string:id>', methods = ['POST','GET'])
+def delete_student(id):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    if 'loggedin' in session:  
+        cur.execute('DELETE FROM post WHERE id = {0}'.format(id))
+        conn.commit()
+        flash('Post Removed Successfully')
+        return redirect(url_for('Index'))
+    return redirect(url_for('login'))
+
 if __name__ == "__main__":
     app.run(debug=True)
